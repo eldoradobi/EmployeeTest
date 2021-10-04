921054cd51ecf6b41a5728a44f5e6d88255155e2
eldoradobi <cherr_y@bigmir.net>
2021-10-04 08:00:07 +0300

    Clear projecn

--- MetinvestTest/Controllers/HomeController.cs
+++ MetinvestTest/Controllers/HomeController.cs
@@ -10,41 +10,10 @@ namespace MetinvestTest.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly AppDbContext db;
-        public HomeController(AppDbContext appDbContext)
-        {
-            this.db = appDbContext;
-        }
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-        [HttpPost]
-        public JsonResult Create(Employee employee)
-        {
-            if (!ModelState.IsValid)
-            {
-               // return BadRequest(ModelState);
-            }
-
-            db.Employees.Add(employee);
-            db.SaveChanges();
-
-            return Json(new { });
-        }
     }
 }
